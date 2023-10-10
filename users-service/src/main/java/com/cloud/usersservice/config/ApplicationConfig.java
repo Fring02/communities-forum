196d49b8f2ade9dd316eed9ae1b347d633c918e3
Султанбек Хасенов <hasenovsultanbek@gmail.com>
2023-10-10 15:51:23 -0400

    Added OpenAPI support

--- users-service/src/main/java/com/cloud/usersservice/config/ApplicationConfig.java
+++ users-service/src/main/java/com/cloud/usersservice/config/ApplicationConfig.java
@@ -3,17 +3,10 @@ package com.cloud.usersservice.config;
 import com.cloud.usersservice.util.DatabaseSeeder;
 import org.modelmapper.ModelMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
